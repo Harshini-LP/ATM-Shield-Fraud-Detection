22442c764df04d753e48f65f86b4254f43574d61
Harshini-LP <nharshini720@gmail.com>
2026-09-19 12:43:05 +0530

    Update database connection settings and improve login

--- index.php
+++ index.php
@@ -2,12 +2,13 @@
 session_start();
 
 // Database Connection Settings (Adjust DB credentials if needed)
-$servername = "localhost";
-$username   = "root";
-$password   = "";
-$dbname     = "atm_fraud";
+$servername = "127.0.0.1";
+$username = "root";
+$password = "";
+$dbname = "atm_fraud";
+$port = 3307;
 
-$conn = new mysqli($servername, $username, $password, $dbname);
+$conn = new mysqli($servername, $username, $password, $dbname, $port);
 
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
